09cda568adc50e17d7447e8f529a6be5a43def9c
Steve Roma <sjroma@hotmail.com>
2017-06-18 11:41:12 -0400

    not much to check in, read, research, then continue

--- main.js
+++ main.js
@@ -1,14 +1,16 @@
 /*
   Here is a guide for the steps you could take:
 */
-
+console.log("talking");
 // 1. First select and store the elements you'll be working with
-
+let search = document.querySelector('.search');
+console.log("searchField:", search);
 
 // 2. Create your `onSubmit` event for getting the user's search term
 
 
 // 3. Create your `fetch` request that is called after a submission
+fetch('https://api.soundcloud.com/tracks?client_id=8538a1744a7fdaa59981232897501e04')
 
 
 // 4. Create a way to append the fetch results to your page
