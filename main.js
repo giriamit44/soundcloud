33774fa5ced3138a4c5c65b76efaaf824b144e58
Steve Roma <sjroma@hotmail.com>
2017-06-19 16:16:28 -0400

    puts results on the page, nothing styled

--- main.js
+++ main.js
@@ -6,11 +6,49 @@ console.log("talking");
 let search = document.querySelector('.search');
 console.log("searchField:", search);
 
+//try and create the search area
+
+
+//try and create the 'Search' button
+//var search_button = document.querySelector(".search-form");
+//var input = document.createElement("input");
+//input.appendChild(form);
+
 // 2. Create your `onSubmit` event for getting the user's search term
 
 
 // 3. Create your `fetch` request that is called after a submission
-fetch('https://api.soundcloud.com/tracks?client_id=8538a1744a7fdaa59981232897501e04')
+//fetch('https://api.soundcloud.com/tracks?client_id=8538a1744a7fdaa59981232897501e04')
+fetch('https://api.soundcloud.com/tracks?client_id=8538a1744a7fdaa59981232897501e04&q=asdf')
+  .then(
+    function(response) {
+      if (response.status !== 200) {
+        console.log('Looks like there was a problem. Status Code: ' +
+          response.status);
+        return;
+      }
+
+      // Examine the text in the response
+      response.json().then(function(data) {
+        //console.log("fetch worked:", data[0].title);
+        for (let i = 0; i < data.length; i++) {
+          console.log("show each title:", data[i].title);
+          let markup = `
+           <div class="title">
+             <p></p>
+             <p></p>
+             <ul>Title and artist:
+               <li>Title: ${data[i].title}</li>
+               <li>User: ${data[i].user.username}</li>
+             </ul>
+           </div>
+         `
+          document.getElementById("results").innerHTML += markup;
+        }
+      });
+    }
+  )
+
 
 
 // 4. Create a way to append the fetch results to your page
