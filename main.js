2c95604e82db429b0a2ac115d6d1ae5b5722b89a
Steve Roma <sjroma@hotmail.com>
2017-06-19 18:30:41 -0400

    small tweak trying to get search input

--- main.js
+++ main.js
@@ -3,25 +3,27 @@
 */
 console.log("talking");
 // 1. First select and store the elements you'll be working with
-
+let search = document.getElementById("search_field").value;
+console.log("what's in search?:", search);
 
 
 // 2. Create your `onSubmit` event for getting the user's search term
-getElementById=submit_button.onclick
+
+//  =submit_button.onclick
 
 // 3. Create your `fetch` request that is called after a submission
 //fetch('https://api.soundcloud.com/tracks?client_id=8538a1744a7fdaa59981232897501e04')
+
 fetch('https://api.soundcloud.com/tracks?client_id=8538a1744a7fdaa59981232897501e04&q=asdf')
   .then(
     function (response) {
       if (response.status !== 200) {
-        console.log('Looks like there was a problem. Status Code: ' +
-          response.status);
+        console.log('Looks like there was a problem. Status Code: ' + response.status);
         return;
       }
 
-// 4. Create a way to append the fetch results to your page
-      // Examine the text in the response
+      // 4. Create a way to append the fetch results to your page
+
       response.json().then(function (data) {
         //console.log("fetch worked:", data[0].title);
         for (let i = 0; i < data.length; i++) {
@@ -40,9 +42,5 @@ fetch('https://api.soundcloud.com/tracks?client_id=8538a1744a7fdaa59981232897501
       });
     }
   )
-
-//<img class="profile_pic" src=${customers.picture.large}>
-
-
 
 // 5. Create a way to listen for a click that will play the song in the audio play
